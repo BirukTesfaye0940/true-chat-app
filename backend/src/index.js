9bb9084282146499fcd27f77b32b54a73c254b21
Yonas Zebrie <yonaszebrie@Biruks-MacBook-Pro.local>
2025-04-23 01:20:22 +0300

    fixed some errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,12 +13,15 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials:true
 }))
+
+app.use(express.json({ limit: '10mb' }));  // or even '20mb' if needed
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)

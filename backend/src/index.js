dcb481fdaeba5ce27acd80acd5490dbc72af3add
Yonas Zebrie <yonaszebrie@Biruks-MacBook-Pro.local>
2025-04-30 11:01:49 +0300

    production ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
+import path from "path";
+
 import { connectDB } from './lib/db.js'
 import cookieParser from "cookie-parser"
 import cors from "cors"
@@ -12,6 +14,7 @@ import { app, server } from './lib/socket.js'
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -25,6 +28,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV ==="production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.send(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`)

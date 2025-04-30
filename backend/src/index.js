4dbe7666d9fc906f346a8d697a26a0d0063c5995
Yonas Zebrie <yonaszebrie@Biruks-MacBook-Pro.local>
2025-04-30 17:04:17 +0300

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-
+console.log
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -25,8 +25,8 @@ app.use(
   })
 );
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/auth/", authRoutes);
+app.use("/api/messages/", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
